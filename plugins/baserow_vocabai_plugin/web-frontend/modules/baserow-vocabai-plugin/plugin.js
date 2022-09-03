8cc97264715a35a0334cbd62a50e0a08e6a123e3
luc <luc.wastiaux@xsmail.com>
2022-09-03 20:00:15 +0800

    register all fields

--- plugins/baserow_vocabai_plugin/web-frontend/modules/baserow-vocabai-plugin/plugin.js
+++ plugins/baserow_vocabai_plugin/web-frontend/modules/baserow-vocabai-plugin/plugin.js
@@ -1,5 +1,8 @@
 import {PluginNamePlugin} from '@baserow-vocabai-plugin/plugins'
 import {LanguageFieldType} from '@baserow-vocabai-plugin/vocabAiFieldTypes'
+import {TranslationFieldType} from '@baserow-vocabai-plugin/vocabAiFieldTypes'
+import {TransliterationFieldType} from '@baserow-vocabai-plugin/vocabAiFieldTypes'
+import {DictionaryLookupFieldType} from '@baserow-vocabai-plugin/vocabAiFieldTypes'
 
 import cloudlanguagetoolsStore from '@baserow-vocabai-plugin/store/cloudlanguagetools'
 
@@ -8,4 +11,7 @@ export default (context) => {
   app.$registry.register('plugin', new PluginNamePlugin(context))
 
   app.$registry.register('field', new LanguageFieldType(context))
+  app.$registry.register('field', new TranslationFieldType(context))
+  app.$registry.register('field', new TransliterationFieldType(context))
+  app.$registry.register('field', new DictionaryLookupFieldType(context))
 }
