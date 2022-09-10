5b68f7f4513e2b216237f2787f7b86a42f48bcd1
luc wastiaux <luc.wastiaux@airpost.net>
2022-09-10 14:39:56 +0800

    script to tag and build docker

---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+VERSION_NUMBER=$1 # for example 0.1
+GIT_TAG=v${VERSION_NUMBER}
+
+git commit -a -m "upgraded version to ${VERSION_NUMBER}"
+git push
+git tag -a ${GIT_TAG} -m "version ${GIT_TAG}"
+git push origin ${GIT_TAG}
+
+# docker build
+docker build -t lucwastiaux/baserow-vocabai-plugin:${VERSION_NUMBER} -f Dockerfile .
+docker push lucwastiaux/baserow-vocabai-plugin:${VERSION_NUMBER}
