b148fb4088bdfab4cd1de4ea1c178935267bbb68
Luc W <lucw@airpost.net>
2022-10-17 11:40:08 +0800

    create user and token

--- plugins/baserow_vocabai_plugin/backend/tests/baserow_vocabai_plugin/cloudlanguagetools/test_clt.py
+++ plugins/baserow_vocabai_plugin/backend/tests/baserow_vocabai_plugin/cloudlanguagetools/test_clt.py
@@ -3,6 +3,6 @@ import pytest
 
 @pytest.mark.django_db
 def test_1(api_client, data_fixture):
-    #user, token = data_fixture.create_user_and_token()
+    user, token = data_fixture.create_user_and_token()
 
     assert False
