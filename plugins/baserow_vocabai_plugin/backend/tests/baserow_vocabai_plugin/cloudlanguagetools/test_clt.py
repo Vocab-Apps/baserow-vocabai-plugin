53a93a3fd37f493c2036569e9083122a24c1ea5e
Luc W <lucw@airpost.net>
2022-10-18 15:40:02 +0800

    ensure that usage records start out at 0

--- plugins/baserow_vocabai_plugin/backend/tests/baserow_vocabai_plugin/cloudlanguagetools/test_clt.py
+++ plugins/baserow_vocabai_plugin/backend/tests/baserow_vocabai_plugin/cloudlanguagetools/test_clt.py
@@ -2,9 +2,9 @@ import pytest
 from django.shortcuts import reverse
 from rest_framework.status import HTTP_200_OK
 
-from baserow_vocabai_plugin.cloudlanguagetools import clt_interface
+from baserow_vocabai_plugin.cloudlanguagetools import clt_interface, quotas
 
-# tests need to be run with LOUDLANGUAGETOOLS_CORE_TEST_SERVICES=yes
+# tests need to be run with CLOUDLANGUAGETOOLS_CORE_TEST_SERVICES=yes
 
 
 @pytest.mark.django_db
@@ -25,3 +25,17 @@ def test_language_data(api_client, data_fixture):
     language_list = response.data
     assert 'fr' in language_list
     assert language_list['fr'] == 'French'
+
+
+@pytest.mark.django_db
+def test_quotas(api_client, data_fixture):
+    user, token = data_fixture.create_user_and_token()
+
+    # update language data first
+    clt_interface.update_language_data()
+
+    # update usage record
+    usage_record = quotas.get_usage_record(user.id)
+    assert usage_record.monthly_usage_record.characters == 0
+    assert usage_record.daily_usage_record.characters == 0
+    # quotas.log_usage
