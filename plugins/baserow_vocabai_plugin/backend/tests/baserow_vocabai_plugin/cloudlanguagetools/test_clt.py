1e7f92c2819ec67f38a1de38250237f2576b99c7
Luc W <lucw@airpost.net>
2022-10-18 15:05:17 +0800

    update language data

--- plugins/baserow_vocabai_plugin/backend/tests/baserow_vocabai_plugin/cloudlanguagetools/test_clt.py
+++ plugins/baserow_vocabai_plugin/backend/tests/baserow_vocabai_plugin/cloudlanguagetools/test_clt.py
@@ -2,11 +2,17 @@ import pytest
 from django.shortcuts import reverse
 from rest_framework.status import HTTP_200_OK
 
+from baserow_vocabai_plugin.cloudlanguagetools import clt_interface
+
 
 @pytest.mark.django_db
 def test_1(api_client, data_fixture):
     user, token = data_fixture.create_user_and_token()
 
+    # update language data first
+    clt_interface.update_language_data()
+
+    # make sure language list is available
     response = api_client.get(
         reverse("api:baserow_vocabai_plugin:language-list"),
         HTTP_AUTHORIZATION=f"JWT {token}",
