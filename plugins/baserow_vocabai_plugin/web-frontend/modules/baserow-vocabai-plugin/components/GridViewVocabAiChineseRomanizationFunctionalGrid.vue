fbf9799419ec025048cf2e92e4051063cfa35c7c
Luc W <lucw@airpost.net>
2023-04-19 09:41:01 +0800

    check if props.value is not null

--- plugins/baserow_vocabai_plugin/web-frontend/modules/baserow-vocabai-plugin/components/GridViewVocabAiChineseRomanizationFunctionalGrid.vue
+++ plugins/baserow_vocabai_plugin/web-frontend/modules/baserow-vocabai-plugin/components/GridViewVocabAiChineseRomanizationFunctionalGrid.vue
@@ -1,6 +1,10 @@
 <template functional>
     <div ref="cell" class="grid-view__cell" :class="data.staticClass || ''">
-      <div class="grid-field-text">functional: {{ props.value }}</div>
+      <div class="grid-field-text">
+        <span v-if="props.value" v-for="word in props.value.solutions">
+          {{ word[0] }}
+        </span>
+      </div>
     </div>
-  </template>
+</template>
   
