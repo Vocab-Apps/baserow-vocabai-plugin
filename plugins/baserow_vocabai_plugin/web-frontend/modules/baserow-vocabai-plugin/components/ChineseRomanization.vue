86503b72c887be26bda02059cad871408954b90d
Luc W <lucw@airpost.net>
2023-04-20 22:23:38 +0800

    show the chosen romanization

--- plugins/baserow_vocabai_plugin/web-frontend/modules/baserow-vocabai-plugin/components/ChineseRomanization.vue
+++ plugins/baserow_vocabai_plugin/web-frontend/modules/baserow-vocabai-plugin/components/ChineseRomanization.vue
@@ -9,7 +9,8 @@ export default {
 <template>
     <span>
         <span v-for="(solution, solution_index) in romanization.solutions">
-            {{ solution[0] }}
+            <span v-if="romanization.solution_overrides.length > solution_index">{{ solution[romanization.solution_overrides[solution_index]] }} </span>
+            <span v-else>{{ solution[0] }} </span>
         </span>
     </span>
 </template>
