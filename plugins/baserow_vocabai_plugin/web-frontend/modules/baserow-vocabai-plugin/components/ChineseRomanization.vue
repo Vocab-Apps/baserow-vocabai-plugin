2113982061adddad97ad4012a10ad967d4eca15c
Luc W <lucw@airpost.net>
2023-04-20 22:30:06 +0800

    working rendering of romanization

--- plugins/baserow_vocabai_plugin/web-frontend/modules/baserow-vocabai-plugin/components/ChineseRomanization.vue
+++ plugins/baserow_vocabai_plugin/web-frontend/modules/baserow-vocabai-plugin/components/ChineseRomanization.vue
@@ -9,7 +9,7 @@ export default {
 <template>
     <span>
         <span v-for="(solution, solution_index) in romanization.solutions">
-            <span v-if="romanization.solution_overrides.length > solution_index">{{ solution[romanization.solution_overrides[solution_index]] }} </span>
+            <span v-if="romanization.solution_overrides[solution_index]">{{ solution[romanization.solution_overrides[solution_index]] }} </span>
             <span v-else>{{ solution[0] }} </span>
         </span>
     </span>
