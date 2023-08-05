98e3cf31df9b20ac40f579aac77ecbd233bcba95
Luc W <lucw@airpost.net>
2023-08-05 10:37:22 +0800

    subscribe users to convertkit

--- plugins/baserow_vocabai_plugin/backend/src/baserow_vocabai_plugin/cloudlanguagetools/tasks.py
+++ plugins/baserow_vocabai_plugin/backend/src/baserow_vocabai_plugin/cloudlanguagetools/tasks.py
@@ -239,14 +239,79 @@ from baserow.core.models import WorkspaceUser
 from baserow.contrib.database.models import Database
 from baserow.contrib.database.table.models import Table
 
+def subscribe_convertkit(user_record_list):
+    convertkit_api_key = os.environ.get('CONVERTKIT_API_KEY', None)
+    convertkit_subscribe = os.environ.get('CONVERTKIT_SUBSCRIBE', 'NO') == 'YES'
+    CONVERTKIT_THROTTLE_REQUESTS_SLEEP = 0.5
+    CONVERTKIT_REQUEST_TIMEOUT=120
+    CONVERTKIT_TAG_ID = 4024166 # vocabai_user
+
+    if convertkit_api_key == None:
+        logger.error(f'CONVERTKIT_API_KEY must be set in order to subscribe users to ConvertKit')
+        return
+
+    # ensure all users are subscribed to convertkit
+    # =============================================
+    try:
+        # build list of emails already subscribed
+        # =======================================
+        url = f'https://api.convertkit.com/v3/tags/{CONVERTKIT_TAG_ID}/subscriptions?api_secret={convertkit_api_key}'
+        response = requests.get(url, timeout=CONVERTKIT_REQUEST_TIMEOUT)
+        time.sleep(CONVERTKIT_THROTTLE_REQUESTS_SLEEP)
+        response.raise_for_status()
+        data = response.data()
+        current_page = data['page']
+        total_pages = data['total_pages']        
+        email_set = set()
+        for item in data['subscriptions']:
+            email_set.add(item['subscriber']['email_address'])
+        # go through all the pages
+        while current_page < total_pages:
+            next_page = current_page + 1
+            url = f'https://api.convertkit.com/v3/tags/{CONVERTKIT_TAG_ID}/subscriptions?api_secret={convertkit_api_key}&page={next_page}'
+            response = requests.get(url, timeout=CONVERTKIT_REQUEST_TIMEOUT)
+            response.raise_for_status()
+            # throttle
+            time.sleep(CONVERTKIT_THROTTLE_REQUESTS_SLEEP)
+            data = response.json()
+            current_page = data['page']
+            for item in data['subscriptions']:
+                email_set.add(item['subscriber']['email_address'])
+
+        # now check each user
+        # ===================
+        for record in user_record_list:
+            email = record['username']
+            first_name = record['name']
+            logger.info(f'processing user: email {email} first_name: {first_name}')
+            if email not in email_set:
+                # subscribe
+                logger.info(f'need to subscribe {email} to convertkit')
+                if convertkit_subscribe:
+                    url = f'https://api.convertkit.com/v3/tags/{CONVERTKIT_TAG_ID}/subscribe'
+                    response = requests.post(url, json={
+                            "api_key": convertkit_api_key,
+                            "email": email,
+                            "first_name": first_name
+                    }, timeout=CONVERTKIT_REQUEST_TIMEOUT)
+                    response.raise_for_status()        
+                    time.sleep(CONVERTKIT_THROTTLE_REQUESTS_SLEEP)
+                else:
+                    logger.info(f'would have subscribed {email} to convertkit')
+    except Exception as e:
+        logger.exception('could not perform convertkit subscriptions')
+    
+
 @app.task(queue='export')
 def collect_user_data():
-    logger.info('collect_user_data')
+    logger.info('running task collect_user_data')
 
     base_url = os.environ.get('BASEROW_USER_STATS_URL', None)
     token = os.environ.get('BASEROW_USER_STATS_TOKEN', None)
+
     if base_url == None or token == None:
-        logger.info(f'BASEROW_USER_STATS_URL or BASEROW_USER_STATS_TOKEN must be set in order to collect user data')
+        logger.error(f'BASEROW_USER_STATS_URL, BASEROW_USER_STATS_TOKEN must be set in order to collect user data')
+        return
 
     user_list = User.objects.all()
 
@@ -254,12 +319,13 @@ def collect_user_data():
 
     for user in user_list:
         # user model: https://docs.djangoproject.com/en/4.1/ref/contrib/auth/
+        # logger.info(f'user: {dir(user)}')
         username = user.username
         last_login = None
         if user.last_login != None:
             last_login = user.last_login.isoformat()
         date_joined = user.date_joined.strftime('%Y-%m-%d')
-        logger.info(f'user: {user} username: {user.username}')
+        logger.info(f'user: {user} first_name: {user.first_name}')
 
         # lookup usage records
         usage_list = VocabAiUsage.objects.filter(user=user)
@@ -292,12 +358,17 @@ def collect_user_data():
 
         user_record_list.append({
             'username': username,
+            'name': user.first_name,
             'last_login': last_login,
             'date_joined': date_joined,
             'table_count': table_count,
             'row_count': row_count
         })
     
+    # make sure all users are subscribed to convertkit
+    # ================================================
+    subscribe_convertkit(user_record_list)
+
     # upload to baserow
     # =================
 
